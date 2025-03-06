13faf053cb43f7e8545536320b95025337771962
koz1tsk1y <koz1tsk1y@yandex.by>
2025-03-06 17:51:20 +0300

    WIP:Сверстать about-section главной страницы

---
+++ src/js/_fancyboxInit.js
@@ -0,0 +1,7 @@
+import { Fancybox } from "@fancyapps/ui";
+
+export function fancyboxInit() {
+  Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+  });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@ import '../scss/style.scss'
 
 import { burgerBtn } from './_burger-btn.js';
 import { counter } from './_counter.js';
+import { fancyboxInit } from './_fancyboxInit.js';
 
 document.addEventListener( 'DOMContentLoaded', function() {
     burgerBtn();
     counter();
+    fancyboxInit();
 })
